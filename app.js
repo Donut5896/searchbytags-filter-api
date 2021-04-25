1f20786e23c73d1698099261ecea00df3794e554
Nutchanokchon Pooyatorn <nutchanokchon5@gmail.com>
2021-04-25 22:37:40 +0700

    -

--- app.js
+++ app.js
@@ -138,7 +138,10 @@ const createFilterBox = (tag) => {
             <div class="filter-tag">
                 <p>${tag}</p>
             </div>
-            <div class="cancel">
+            <div class="filter-remove-icon">
+                <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14">
+                <path fill="#fff" fill-rule="evenodd" d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"/>
+                </svg>
             </div>
         </div>    
 
