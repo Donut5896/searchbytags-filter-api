fae4c997af6cfb9746421b504acd879affe6fb57
Nutchanokchon Pooyatorn <nutchanokchon5@gmail.com>
2021-04-26 21:52:06 +0700

    fix filter issues

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const items = document.querySelector(".lists");
 const searchBar = document.querySelector(".searchBar");
-const searchTabs = document.querySelector(".searchTag");
+
 
 let  searchTrips = [];
 
@@ -73,7 +73,9 @@ const loadTrips = async() => {
 
 
 
-
+ /*<div class="image-card">
+                        <img src="${trip.photos[0]}" alt="" />
+                 </div*/
 
 
 
@@ -86,10 +88,19 @@ const showTrips = (trips) => {
 
         return (
          items.insertAdjacentHTML('beforeend', ` 
-            <li class="container-list" id="${trip.id}">
-                 <div class="image-card">
-                    <img src=${trip.photos[0]} alt="" />
-                 </div
+            <div class="container-list" id="${trip.eid}">
+
+                   <div class="images-slide">
+                            <div class="slide">
+                                ${trip.photos.map(photo => {
+                                    return `<img src="${photo}" />
+                                  `
+                                }).join('')}
+                               
+                            </div>  
+                      
+        
+
                 <div class="contents">
                     <div class="content">
                         <div class="title">
@@ -102,40 +113,36 @@ const showTrips = (trips) => {
                             </a>
                         </div>
 
-                        <div class="hashtags" id="hashtags">
-                            <h5>หมวดหมู่: </h5>
-                            <span class="tag" id="dots">
-                                <div class="collapse_ " id="collapseTag"> 
+                        <div class="hashtags" >
+                                    <h5>หมวดหมู่: </h5>
+                                   <div id="dots">
+
+                                    <span class="collapse_" id="collapseTag" aria-expanded="false">
                                     ${trip.tags.map(tag => {
-                                        return `<div class="job-tag" data-value="${tag}">${tag}</div>`
-                                    }).join('')}
-                                </div>
-                            </span>
+                                        return `<div class="job-tag" data-value="${tag}"><p class="txt">${tag}<p></div>`
+                                    })}
+                                    </span>
+                                    <a role="button" class="collapseDot" data-toggle="collapse" href="${trip.url}" aria-expanded="false"> 
+                                    </div>
+                            </a>
                         </div>
 
-                        <div class="images-slide">
-                            <div class="slide">
-                                <img src=${trip.photos[1]} />
-                                <img src=${trip.photos[2]} />
-                                <img src=${trip.photos[3]} />
-                            </div>  
-                        </div>
+                     
                     </div> 
                 </div>
-            </li>`)
+                 
+            </div>     
+                 </div>`)
     )}).join('')
 
-//items.innerHTML = tripListingHTML;
-
 };
-
 
 
 //create tag on filter bar
 const createFilterBox = (tag) => {
     document.querySelector('.filter-lists').insertAdjacentHTML("beforeend", `
-        <div class="filter-list-box" data-value="${tag}">
-            <div class="filter-tag">
+        <div class="filter-list-box"  data-value="${tag}">
+            <div class="tabs">
                 <p>${tag}</p>
             </div>
             <div class="filter-remove-icon">
@@ -143,7 +150,8 @@ const createFilterBox = (tag) => {
                 <path fill="#fff" fill-rule="evenodd" d="M11.314 0l2.121 2.121-4.596 4.596 4.596 4.597-2.121 2.121-4.597-4.596-4.596 4.596L0 11.314l4.596-4.597L0 2.121 2.121 0l4.596 4.596L11.314 0z"/>
                 </svg>
             </div>
-        </div>    
+           
+        </div>
 
     `)
 }
@@ -151,8 +159,7 @@ const createFilterBox = (tag) => {
 //remove tag from filter bar
 const removefilterBox = (tag) => {
      document.querySelectorAll('filter-list-box').forEach(index => {
-         if(index.getAttribute('data-value') == tag)
-          index.remove();
+         if(index.getAttribute('data-value') == tag) index.remove();
      })
 }
 
@@ -160,11 +167,14 @@ const removefilterBox = (tag) => {
 
 //open and close effect
 function openFilterbox(){
-    document.querySelector('.searchTag').style.transform= "translateY(-3.5rem) scaleY(1)"
+    document.querySelector('.filter-wrapper').style.transform= "translateY(-3.5rem) scaleY(1)"
 }
 function closeFilterbox(){
-    document.querySelector('.searchTag').style.transform = "translateY(-3.5rem) scaleY(0)"
+    document.querySelector('.filter-wrapper').style.transform = "translateY(-3.5rem) scaleY(0)"
 }
+
+
+
 
 
 //clear all
@@ -181,10 +191,13 @@ let store = [];
 
 //job list bar
 window.addEventListener('click', (e) => {
-    const jobTagGroup = document.querySelectorAll('.collapse_ ');
+    const jobTagGroup = document.querySelectorAll('.hashtags');
     const filterTags = e.target.closest('.filter-list-box');
     const tagText = e.target.textContent.trim()
 
+    console.log(jobTagGroup)
+    console.log(filterTags)
+    console.log(tagText)
     //push tag to storage, which is filter list
     if(e.target.closest('.job-tag')){
         openFilterbox();
@@ -203,8 +216,10 @@ window.addEventListener('click', (e) => {
         removefilterBox((filterTags.childNodes[1]).textContent);
         const storeIndex = store.indexOf(filterTags.childNodes[1].textContent)
         store.splice(storeIndex, 1)
+        
+        console.log(storeIndex)
     }
-
+  
 
     //joblist display controller
     jobTagGroup.forEach(index => {
@@ -212,27 +227,32 @@ window.addEventListener('click', (e) => {
         jobListingContainer.style.display = "none";
 
         //convert tags list into readable string arrays
-        const tagArrays = index.textContent.replace(/\s+/g, ' ').trim().split(' ');
-
+        const tagArrays = index.textContent.replace(/\s+/g, ' ').trim().split(',');
+        console.log(index)
         //store can be found on the first 'if condition' inside eventListener
         if(store.every(each => tagArrays.includes(each))){
             jobListingContainer.classList.add('marked');
             jobListingContainer.style.display = "flex";
+            
         }else{
             jobListingContainer.classList.remove('marked')
         }
+        console.log(jobListingContainer)
+        console.log(tagArrays)
+        
     });
 
 
     //clearAll button on filter bar
     if(e.target.closest('.filter-wrapper')){
-        const filterLists = documnet.querySelectorAll('.filter-list-box');
+        const filterLists = document.querySelectorAll('.filter-list-box');
 
         if(e.target.textContent == 'Clear'){
             filterLists.forEach(index => index.remove());
             store = [];
             clearAll();
         }
+        console.log(filterLists)
     }
 
     //check if filter box is empty
@@ -256,27 +276,6 @@ loadTrips();
 
 
 
-// buttonTags.forEach( (item) => {
-//             const button = document.createElement("button");
-//             button.innerHTML = item;
-//             items.appendChild(button);
-
-      
-//            button.addEventListener("click", (e) => {
-//             items.innerHTML = "";
-//             const targetText = e.target.innerHTML;
-//             filterTags.push(targetText);
-
-//             //array for clicked tags 
-//             filterTags = [...new Set(filterTags)];
-        
-//             setTripList(filterTags);
-           
-//         }) 
-        
-        
-      
-//     })
 
 
 
@@ -286,28 +285,6 @@ loadTrips();
 
 
 
-/*//3.filter job list
-function setTripList(filter){
-      if(!filter || !filter.length){
-          items.innerHTML = "";
-          return searchTrips.forEach((item) => {
-              return showTrips(item);  
-          })
-       
-      }else if(filter){
-        filter.forEach((word) => {
-            items.innerHTML = "";
-            setTabs(filter);
-            return  searchlocation(word).forEach( (item)=> {
-                return[...new Set (showTrips(item))];
-                      
-            });
-          
-        });
-  
-    }
-  
-}
 
 
 
@@ -315,23 +292,11 @@ function setTripList(filter){
 
 
 
-//2.set tab based on each input
-
-function setTabs(tab){
-      console.log(tab)
-    searchTabs.innerHTML = "";
-    return tab.forEach((item) => {
-        createTaps(item)
-    })
-  
-}
 
 
 
-setTripList();
 
-
-// 1.create tags and append to searchtabs
+/* 1.create tags and append to searchtabs
 function createTaps(item){
     const tabEl = document.createElement("div");
     tabEl.classList.add("tabs");
@@ -359,36 +324,10 @@ function createTaps(item){
     return [searchTabs.appendChild(tabEl)];
 }
 
-
-
-
-function ccc(text){
-    filterTags = filterTags.filter((item) => {
-        if(item != text){
-            return item
-        }
-   
-    }) 
-    return [...filterTags];
- 
-}
-
-
-
-
-function searchlocation(word){
-   
-    searchTrips = searchTrips.filter((item) => {
-        if(
-             item.tags.includes(word)
-        ){
-            return item
-            console.log(item);
-        }
-    })
-    return searchTrips
-   
-}
-
-
 */
+
+
+
+
+
+
